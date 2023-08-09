60dbb9af1c5a2d6429bc8a0c498c778d1d6342ee
Andrew <Andy>
2023-08-09 22:24:32 +0300

    DZ2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+while (chislo > 999)
+{
+ chislo = chislo / 10;
+}
+ if (chislo < 1000 && chislo > 99)
+{
+   Console.Write("Третье число -> " + chislo % 10); 
+}
+ else
+{
+    Console.Write("Третьей цифры нет");
+}
